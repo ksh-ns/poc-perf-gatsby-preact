b025b45e831f9629526e8a70df6583267bb9eb61
Karolis Šarapnickis <73160522+ksh-ns@users.noreply.github.com>
2021-01-06 11:41:56 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby + Node.js (TypeScript) API",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Gatsby + Node.js (TypeScript) API",
+        short_name: "Gatsby + Node.js (TypeScript)",
+        start_url: "/",
+      },
+    },
+  ],
+};
